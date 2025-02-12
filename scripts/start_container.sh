0fa057d7bebdcfd71a92ee59651fcfd92a966c93
Ganga Manikanta Reddy Mooli <159711293+mani9704@users.noreply.github.com>
2025-02-11 22:18:15 -0600

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull manikantareddy9704/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 8000:8000 abhishekf5/simple-python-flask-app
+docker run -d -p 8000:8000 manikantareddy9704/simple-python-flask-app
