9139ebb37fef6832e7355b8421f02de88f382104
Ganga Manikanta Reddy Mooli <159711293+mani9704@users.noreply.github.com>
2025-02-11 21:45:58 -0600

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-
+container_name_pattern=$1
+# Get container ID(s) matching the name pattern
+containerid=$(docker ps -q -f "name=$container_name_pattern")
 # Stop the running container (if any)
-containerid=$(docker ps -q -f "name=$1")
 docker rm $containerid
